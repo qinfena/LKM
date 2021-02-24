b40a9481e68034f6461743656459f19870f6c83e
qinfena <xiangxianjun@hba.cn>
2021-02-24 14:15:06 +0800

    new http.c

--- get_http/http.c
+++ get_http/http.c
@@ -9,19 +9,30 @@
 
 MODULE_LICENSE("GPL");
 
+#define NIPQUAD(addr) \
+  ((unsigned char *)&addr)[0], \
+  ((unsigned char *)&addr)[1], \
+  ((unsigned char *)&addr)[2], \
+  ((unsigned char *)&addr)[3]
+
 struct nf_hook_ops post_hook;    /* Outgoing */
+#if 0
 static int have_pair = 0;	/* Marks if we already have pair */
+#endif
 
 static unsigned int watch_out(void *priv, struct sk_buff *skb,\
 								const struct nf_hook_state *state) {
 	struct sk_buff *sb = skb;
 	struct tcphdr *tcp;
+	struct iphdr * iph;
+
 	//printk("post routing");
     /* Make sure this is a TCP packet first */
 	if (ip_hdr(sb)->protocol != IPPROTO_TCP)
 		return NF_ACCEPT;    /* Nope, not TCP */
 		
 	tcp = (struct tcphdr *)((sb->data) + (ip_hdr(sb)->ihl * 4));
+	iph = ip_hdr(sb);
 
     /* Now check to see if it's an HTTP packet */
 	if (tcp->dest != htons(80))
@@ -29,9 +40,11 @@ static unsigned int watch_out(void *priv, struct sk_buff *skb,\
 
     /* Parse the HTTP packet for relevant information if we don't already
 	 * have a username and passwd pair. */
-	 if (!have_pair) {
-		 printk("check http");
-	 }
+	 //if (!have_pair) {
+
+		 //printk("check http dest : %d, source : %d ", tcp->dest, tcp->source);
+		 printk("check http source : %d.%d.%d.%d:%d, dest : %d.%d.%d.%d:%d ", NIPQUAD(iph->saddr), tcp->source, NIPQUAD(iph->daddr), tcp->dest);
+	 //}
     /* We are finished with the packet, let it go on its way */
 	return NF_ACCEPT;
 }
